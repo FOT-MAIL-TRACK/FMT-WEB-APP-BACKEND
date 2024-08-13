1249d52ea147f693cb9015c561be6467986fb770
Madusha Lakshan <madushalakshan026@gmail.com>
2024-08-13 22:09:54 +0530

    signinpage updated

---
+++ frontend/mail-tracking-system/src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import usjlogo from '../assets/usjp-logo.png'
+import './NavBar.css';
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+    <div className="navbar-content">
+      <img src={usjlogo} alt="University Logo" className="navbar-logo" />
+      <h1 className="navbar-text">University of Sri Jayawardenepura</h1>
+    </div>
+  </div>
+  );
+}
+
+export default NavBar;
