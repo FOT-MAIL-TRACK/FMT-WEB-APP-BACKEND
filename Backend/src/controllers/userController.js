ff7867d1da6cdbf87ff20af9cd4513d2cd12b356
Madusha Lakshan <madushalakshan026@gmail.com>
2024-10-08 20:29:16 +0530

    Connected frontend and backend user registration and signin functioanlity using axios

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -21,7 +21,7 @@ exports.signin = async (req,res) => {
         if (!user || !(await user.comparePassword(password))) {
             return res.status(401).json({mwssage: 'Invalid email or password'});
         }
-        const token = jwt.sign({userId: user._id, role: user.role},process.env.JWT_SECRET, { expiresIn: '1h'});
+        const token = jwt.sign({userId: user._id, role: user.role},process.env.JWT_SECRET, { expiresIn: '2h'});
         // res.json({ token, user: { id: user._id, name: user.name, username: user.username, email: user.email, role: user.role, faculty: user.faculty, department: user.department, registrationNumber: user.registrationNumber}});
         res.json({ token, user: { id: user._id, name: user.name, username: user.username, email: user.email, role: user.role, faculty: user.faculty,  registrationNumber: user.registrationNumber}});
     }catch( error) {
