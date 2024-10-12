58d202bf4a1c86212a207f0e50ad7d4e1e910d2c
Samoda Charana <142619901+SamodaCharana@users.noreply.github.com>
2024-10-12 10:20:20 +0530

    Update server.test.js
    Signed-off-by: Samoda Charana <142619901+SamodaCharana@users.noreply.github.com>

--- Backend/__tests__/server.test.js
+++ Backend/__tests__/server.test.js
@@ -1,1 +1,10 @@
+const request = require('supertest');
+const app = require('../src/server');
+
+describe('GET /api/example', () => {
+  it('should return 200 OK', async () => {
+    const res = await request(app).get('/api/example');
+    expect(res.statusCode).toEqual(200);
+  });
+});
 
